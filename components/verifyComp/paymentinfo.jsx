bcc113982c0179191848c6d0c84dcdc32a1c59e5
chricmako6 <156549010+chricmako6@users.noreply.github.com>
2025-12-13 06:51:48 -0500

    updating the tab title

---
+++ components/verifyComp/paymentinfo.jsx
@@ -0,0 +1,12 @@
+'use client';
+
+import React from 'react';
+
+export default function PaymentInfo() {
+  return (
+    <div className="payment-info">
+      <h2>Payment Information</h2>
+      <p>Payment details will be added here</p>
+    </div>
+  );
+}
